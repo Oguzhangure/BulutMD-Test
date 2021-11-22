585d02d394f94de1b7ee58d87a6ea2ae18d15d2e
Oğuzhan Güre <oguzhan.gure@outlook.com>
2021-11-23 00:59:33 +0300

    added(Item Component)

---
+++ src/components/ItemInfobox.jsx
@@ -0,0 +1,55 @@
+//REACT IMPORTS
+import { useState } from "react";
+import React from "react";
+//CHAKRA IMPORTS
+import { Flex, Image, Heading, Collapse, Button } from "@chakra-ui/react";
+//COMPONENTS
+
+function ItemInfobox({ description, year, src, title, ...probs }) {
+  const [show, setShow] = useState(false);
+
+  const handleToggle = () => setShow(!show);
+  return (
+    <Flex
+      border="2px"
+      w="215px"
+      borderRadius="15px"
+      padding="8px"
+      flexDirection="column"
+      background="#b5838d"
+      boxShadow="lg"
+    >
+      <Image
+        alignSelf="center"
+        border="1px"
+        boxShadow="md"
+        src={src}
+        maxW="200px"
+        borderRadius="15px"
+      ></Image>
+      <Heading textAlign="center" size="lg">
+        {title}
+      </Heading>
+      <Heading textAlign="center" size="md">
+        {year}
+      </Heading>
+      <Collapse startingHeight={45} in={show}>
+        {description}
+      </Collapse>
+      <Button
+        _hover={{
+          backgroundColor: "#ffb4a2",
+        }}
+        _focus="none"
+        variant="ghost"
+        size="sm"
+        onClick={handleToggle}
+        mt="1rem"
+      >
+        Ayrıntıları {show ? "Gizle" : "Göster"}
+      </Button>
+    </Flex>
+  );
+}
+
+export default ItemInfobox;
